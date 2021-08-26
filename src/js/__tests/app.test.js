040cd34fac2287de411739b73333fc0d22530bf8
Polina Filippova <polina17012001@mail.ru>
2021-08-26 14:45:06 +0300

    version1

---
+++ src/js/__tests/app.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+test('check sum value', () => {
+  expect(5).toBeGreaterThan(4) // больше чем
+  expect(5).toBeGreaterThanOrEqual(5) // больше или равно
+  expect(5).toBeLessThan(10) // меньше
+  expect(5).toBeLessThanOrEqual(5) // меньше или равно
+});
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+// TODO: write code here
+import addField from './field';
+import addImage from './image';
+
+addField(4);
+setInterval(() => addImage(4), 2000);
